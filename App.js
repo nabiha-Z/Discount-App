c8f3610e81af92c4df2fd94cea253b538b2cc438
nabiha-Z <nabihazubair100@gmail.com>
2021-04-24 01:50:30 +0500

    Styling and Exception

--- App.js
+++ App.js
@@ -7,12 +7,58 @@ const [getOriginal, setOriginal] = useState(0);
 const [getDicount, setDisocunt] = useState(0);
 const [getSave, setSave] = useState(0);
 const[getFinalPrice, setFinalPrice] = useState(0);
+const[getErrorMsg, setErrorMsg] = useState(Array(2).fill(""));
 function calculateDicount (){
-
+if(getOriginal ==0 ){
+  setFinalPrice("---");
+  setSave("---");
+}else{
  var discount = getOriginal * (getDicount/100);
+ discount=discount.toFixed(2);
  setSave(discount);
+ 
  var finalP=getOriginal - discount;
+ finalP=finalP.toFixed(2);
  setFinalPrice(finalP);
+}
+}
+
+function PositivePrice(text){
+  if(text<0){
+    setErrorMsg((getErrorMsg) => {
+                getErrorMsg[0] = "Price should be a positive number."
+                return getErrorMsg;
+            });
+    setOriginal(0);
+    setFinalPrice("---");
+  }else{
+    setOriginal(text);
+     setErrorMsg((getErrorMsg) => {
+                getErrorMsg[0] = ""
+                return getErrorMsg;
+            });
+      
+    
+  }
+
+}
+
+function Validation(t){
+  if(t>100){
+
+    setErrorMsg((getErrorMsg) => {
+                getErrorMsg[1] = "Discount Value should not be greater than 100"
+                return getErrorMsg;
+            });
+    setDisocunt(0);
+  }else{
+    setDisocunt(t);
+    setErrorMsg((getErrorMsg) => {
+                getErrorMsg[1] = ""
+                return getErrorMsg;
+            });
+  }
+
 
 }
 
@@ -29,18 +75,22 @@ useEffect(() =>{
       keyboardType = 'number-pad'
       style={styles.inputField}
       placeholder='Enter Price Here'
-      onChangeText={text => setOriginal(text)}
+      onChangeText={text => PositivePrice(text)}
       />
       
       </View>
+      <Text style={{fontSize:15, color:'red', margin:5}}>{getErrorMsg[0]}</Text>
       <View style={styles.ViewContainer}>
       <Text style={styles.text}>Disount (%)</Text>
       <TextInput 
       style={styles.inputField}
+      keyboardType = 'number-pad'
       placeholder='Enter Price Here'
-      onChangeText={text => setDisocunt(text)}
+      maxLength={3}
+      onChangeText={text => Validation(text)}
       />
       </View>
+      <Text style={{fontSize:15, color:'red', margin:5}}>{getErrorMsg[1]}</Text>
       <Text style={styles.finalPriceText}>You Save: Rs. {getSave}</Text>
       <Text style={styles.finalPriceText}>Final Price</Text>
       <View style={styles.dicount}>
